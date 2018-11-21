18eca6958cf14a6f5fd615f2ccc664b0cabee63e
Suchin Gururangan <github@suchin.io>
2018-11-20 21:31:16 -0800

    push

--- models/vae_classifier.py
+++ models/vae_classifier.py
@@ -10,14 +10,27 @@ from overrides import overrides
 from allennlp.training.metrics import CategoricalAccuracy, Average
 from allennlp.modules import FeedForward
 
-@Model.register("vae_classify_encoder")
-class VAE_ENCODER_CLF(Model):
+@Model.register("vae_classifier")
+class VAE_CLF(Model):
+    """
+    Perform text classification with a VAE
+
+    Params
+    ______
+
+    vocab: ``Vocabulary``
+        vocabulary
+    vae : ``VAE``
+        variational autoencoder (RNN or BOW-based)
+    classifier: ``FeedForward``
+        feedforward network classifying input
+    """
     def __init__(self, 
                  vocab: Vocabulary,
                  vae: VAE,
                  classifier: FeedForward,
                  initializer: InitializerApplicator = InitializerApplicator()):
-        super(VAE_ENCODER_CLF, self).__init__(vocab)
+        super(VAE_DECODER_CLF, self).__init__(vocab)
         self.metrics = {
             'kld': Average(),
             'reconstruction': Average(),
@@ -35,28 +48,40 @@ class VAE_ENCODER_CLF(Model):
     @overrides
     def forward(self, tokens, label):  # pylint: disable=W0221
         """
-        Given an input vector, produces the latent encoding z, followed by the mean and
-        log variance of the variational distribution produced.
-
-        z is the result of the reparameterization trick (Autoencoding Variational Bayes (Kingma et al.)).
+        Given tokens and labels, generate document representation with
+        a latent code and classify.
         """
+
+        # run VAE to decode with a latent code
         vae_output = self._vae(tokens, label)
-        output = self._classifier(vae_output['cont_repr'])
+
+        if self._vae.name == 'rnn_vae':
+            clf_input = vae_output['decoded_output']
+            document_vectors = torch.max(x_recon, 1)[0]
+        else:
+            clf_input = vae_output['decoded_output'].squeeze(0)
+
+        # classify
+        output = self._classifier(clf_input)
         logits = self._output_logits(output)
         discriminator_loss = self._discriminator_loss(logits, label)
 
+
+        # set metrics
         reconstruction_loss = vae_output['reconstruction']
         elbo = vae_output['elbo']
         kld = vae_output['kld']
         nll = vae_output['nll']
-        clf_output = vae_output
-        clf_output['loss'] = vae_output['elbo'] + discriminator_loss
-        # set metrics
         self.metrics['accuracy'](logits, label)
         self.metrics["reconstruction"](reconstruction_loss.mean())
         self.metrics["elbo"](elbo.mean())
         self.metrics["kld"](kld.mean())
         self.metrics["nll"](nll.mean())
+
+        # create clf_output
+        clf_output = vae_output
+        clf_output['loss'] = vae_output['elbo'] + discriminator_loss
+
         return clf_output
 
     def get_metrics(self, reset: bool = False) -> Dict[str, float]:
