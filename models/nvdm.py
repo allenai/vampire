71e51f01448acc477059ded8d540534f48d0f890
Suchin Gururangan <github@suchin.io>
2019-01-02 14:43:01 -0500

    update

--- models/nvdm.py
+++ models/nvdm.py
@@ -43,8 +43,11 @@ class NVDM(Model):
                  kl_weight_annealing: str = None,
                  dropout: float = 0.5,
                  track_topics: bool = False,
+                 topic_log_interval: int = 100,
                  tie_weights: bool = False,
                  classifier: Classifier = None,
+                 pretrained_file: str = None,
+                 freeze_weights: bool = False,
                  initializer: InitializerApplicator = InitializerApplicator()) -> None:
         super(NVDM, self).__init__(vocab)
 
@@ -80,7 +83,7 @@ class NVDM(Model):
         self._dist = distribution
         self.hidden_dim = hidden_dim
         self.latent_dim = latent_dim
-        
+        self.topic_log_interval = topic_log_interval
         if continuous_embedder is not None:
             self.embedding_dim = continuous_embedder.token_embedder_tokens.get_output_dim()
         else:
@@ -126,8 +129,12 @@ class NVDM(Model):
         self._dist._initialize_params(param_input_dim, latent_dim)
 
         self._decoder._initialize_decoder_out(latent_dim, vocab.get_vocab_size(self.vocab_namespace))
-
-        initializer(self)
+        
+        if pretrained_file is not None:
+            archive = load_archive(pretrained_file)
+            self._initialize_weights_from_archive(archive, freeze_weights)
+        else:
+            initializer(self)
 
     def _initialize_weights_from_archive(self,
                                          archive: Archive,
@@ -144,11 +151,9 @@ class NVDM(Model):
         archived_parameters = dict(archive.model.named_parameters())
         for item, val in archived_parameters.items():
             new_weights = val.data
-            item_sub = ".".join(item.split('.')[1:])
-            model_parameters[item_sub].data.copy_(new_weights)
-            if freeze_weights:
-                item_sub = ".".join(item.split('.')[1:])
-                model_parameters[item_sub].requires_grad = False
+            model_parameters[item].data.copy_(new_weights)
+            if freeze_weights and "classifier" not in item:
+                model_parameters[item].requires_grad = False
     
     def _freeze_weights(self) -> None:
         """
@@ -223,21 +228,13 @@ class NVDM(Model):
         return topics
 
     def run(self,
-                tokens: Dict[str, torch.Tensor],
-                targets: Dict[str, torch.Tensor]=None,
-                label: torch.IntTensor=None) -> Dict[str, torch.Tensor]:
+            tokens: Dict[str, torch.Tensor],
+            targets: Dict[str, torch.Tensor]=None,
+            label: torch.IntTensor=None) -> Dict[str, torch.Tensor]:
         """
         Run one step of VAE with RNN decoder
         """
-
-        if not self.training:
-            self.weight_scheduler = lambda x: 1.0
-            self._dist._apply_batchnorm = self.dist_apply_batchnorm
-            self._decoder._apply_batchnorm = self.decoder_apply_batchnorm
-        else:
-            self.weight_scheduler = lambda x: schedule(x, self.kl_weight_annealing)
-            self._encoder._apply_batchnorm = False
-            self._decoder._apply_batchnorm = False
+        
         output = {}
         input_repr = []
         batch_size, seq_len = tokens['tokens'].shape
@@ -306,8 +303,8 @@ class NVDM(Model):
                 output['clf_loss'] = clf_output['loss']
                 output['logits'] = clf_output['logits']
 
-        if self.track_topics:
-            if self.step == 100:
+        if self.track_topics and self.training:
+            if self.step == self.topic_log_interval:
                 topics = self.extract_topics()
                 # self.metrics['npmi'](self.compute_npmi(topics))
                 tqdm.write(tabulate(topics))
@@ -328,7 +325,7 @@ class NVDM(Model):
 
         if labeled_output is not None:
             output['clf_loss'].append(labeled_output['clf_loss'])
-            output['logits'].append(labeled_output['logits'])
+            output['logits'] = labeled_output['logits']
             output['elbo'].append(labeled_output['elbo'])
             output['kld'].append(labeled_output['kld'])
             output['nll'].append(labeled_output['nll'])
@@ -336,7 +333,8 @@ class NVDM(Model):
             output['kld_weight'].append(labeled_output['kld_weight'])
 
         for key, item in output.items():
-            output[key] = sum(item) / len(item)
+            if key != 'logits':
+                output[key] = sum(item) / len(item)
         return output
 
     def forward(self,
@@ -344,16 +342,27 @@ class NVDM(Model):
                 is_labeled: torch.IntTensor,
                 targets: Dict[str, torch.Tensor]=None,
                 label: torch.IntTensor=None) -> Dict[str, torch.Tensor]: 
-        
+        if not self.training:
+            self.weight_scheduler = lambda x: 1.0
+            self._dist._apply_batchnorm = self.dist_apply_batchnorm
+            self._decoder._apply_batchnorm = self.decoder_apply_batchnorm
+        else:
+            self.weight_scheduler = lambda x: schedule(x, self.kl_weight_annealing)
+            self._encoder._apply_batchnorm = False
+            self._decoder._apply_batchnorm = False
+
         is_labeled_tokens=torch.Tensor(np.array([int(self.vocab.get_token_from_index(x.item(), namespace="is_labeled")) for x in is_labeled]))
         supervised_instances, unsupervised_instances = split_instances(tokens=tokens, label=label, is_labeled=is_labeled_tokens, targets=targets)
-
-        if supervised_instances.get('tokens') is not None:
+        
+        if (supervised_instances.get('tokens') is not None and supervised_instances['tokens']['tokens'].shape[0] > 1 
+            or supervised_instances.get('tokens') is not None and not self.training):
             labeled_output = self.run(**supervised_instances)
+            clf = 1
         else:
+            clf = 0
             labeled_output = None
 
-        if unsupervised_instances.get('tokens') is not None:
+        if unsupervised_instances.get('tokens') is not None and unsupervised_instances['tokens']['tokens'].shape[0] > 1:
             unlabeled_output = self.run(**unsupervised_instances)
         else:
             unlabeled_output = None
@@ -367,7 +376,7 @@ class NVDM(Model):
         self.metrics["perp"] = float(np.exp(self.metrics['nll'].get_metric()))
         self.metrics["cos"] = output['avg_cos']
 
-        if self._classifier is not None and supervised_instances.get('tokens') is not None:
+        if self._classifier is not None and clf == 1:
             self.metrics['accuracy'](output['logits'], supervised_instances['label'])
         
         output['loss'] = output['elbo']
