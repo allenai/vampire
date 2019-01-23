6ce7d4dcab6aae5ed5517b4d8c48603b4103665c
Suchin Gururangan <github@suchin.io>
2019-01-23 12:43:12 -0800

    added bins

--- bin/train_classifier.sh
+++ bin/train_classifier.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-config=$1
-override=$2
+ser_dir=$1
+config=$2
+override=$3
 
-serialization_dir=model_logs/${config}
+serialization_dir=model_logs/${ser_dir}
 training_config=training_config/${config}.json
 
 if [ "$override" == "override" ]; then
@@ -13,8 +14,10 @@ fi
 
 allennlp train \
     --include-package models.baselines.logistic_regression \
-    --include-package dataset_readers.semisupervised_text_classification_json \
+    --include-package data.dataset_readers.semisupervised_text_classification_json \
     --include-package common.allennlp_bridge \
     --include-package modules.token_embedders.vae_token_embedder \
+    --include-package models.nvdm
+    --include-package data.tokenizers.regex_and_stopword_filter
     -s $serialization_dir \
     $training_config
