a495963e95d204e3aad3e77b9a30de1ed0302000
Suchin Gururangan <github@suchin.io>
2019-01-23 13:04:00 -0800

    added bin

--- bin/train_clf.sh
+++ bin/train_clf.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 ser_dir=$1
-config=$2
-override=$3
+model=$2
+vae=$3
+override=$4
 
 serialization_dir=model_logs/${ser_dir}
-training_config=training_config/${config}.json
+if [ "$vae" == "vae" ]; then
+  training_config=training_config/baselines/${model}_vae.json
+else;
+  training_config=training_config/baselines/${model}.json
+fi
+
+training_config=training_config/baselines/${config}.json
 
 if [ "$override" == "override" ]; then
   echo "overriding $serialization_dir..."
@@ -13,11 +20,10 @@ if [ "$override" == "override" ]; then
 fi
 
 allennlp train \
-    --include-package models.baselines.logistic_regression \
+    --include-package models.baselines.${model} \
     --include-package data.dataset_readers.semisupervised_text_classification_json \
     --include-package common.allennlp_bridge \
     --include-package modules.token_embedders.vae_token_embedder \
-    --include-package models.nvdm \
     --include-package data.tokenizers.regex_and_stopword_filter \
     -s $serialization_dir \
     $training_config
