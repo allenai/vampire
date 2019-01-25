bdff4e549446feaee3027772301d3a10be861581
Suchin Gururangan <github@suchin.io>
2019-01-24 22:12:02 -0800

    added tests, >90% coverage

--- vae/modules/distribution/__init__.py
+++ vae/modules/distribution/__init__.py
@@ -1,4 +1,3 @@
 from vae.modules.distribution.gaussian import Gaussian
 from vae.modules.distribution.logistic_normal import LogisticNormal
-from vae.modules.distribution.vmf import VMF
 from vae.modules.distribution.distribution import Distribution
