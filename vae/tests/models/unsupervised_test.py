37cea6fc32c923ff0d700004563f8c9b43afb8fe
Suchin Gururangan <github@suchin.io>
2019-02-04 13:05:03 -0800

    added npmi update

---
+++ vae/tests/models/unsupervised_test.py
@@ -0,0 +1,70 @@
+# pylint: disable=no-self-use,invalid-name,unused-import
+import numpy as np
+from allennlp.commands.train import train_model_from_file
+from allennlp.common.testing import ModelTestCase
+
+from vae.common.allennlp_bridge import VocabularyBGDumper
+from vae.common.testing.test_case import VAETestCase
+from vae.data.dataset_readers import SemiSupervisedTextClassificationJsonReader
+from vae.data.tokenizers import regex_and_stopword_filter
+from vae.models import unsupervised
+
+
+class TestUnsupervised(ModelTestCase):
+    def setUp(self):
+        super(TestUnsupervised, self).setUp()
+        self.set_up_model(VAETestCase.FIXTURES_ROOT / 'nvdm' / 'experiment_logistic.json',
+                          VAETestCase.FIXTURES_ROOT / "imdb" / "train.jsonl")
+
+    def test_model_can_train_save_and_load_unsupervised(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
+        topics = [(1, ["great", "movie"]), (2, ["bad", "film"])]
+        
+        npmi =  self.model.compute_npmi(topics, num_words=30)
+
+        ref_vocab = self.model._ref_vocab
+        ref_counts = self.model._ref_counts
+
+        vocab_index = dict(zip(ref_vocab, range(len(ref_vocab))))
+        n_docs, _ = ref_counts.shape
+
+        npmi_means = []
+        for topic in topics:
+            words = topic[1]
+            npmi_vals = []
+            for word_i, word1 in enumerate(words[:30]):
+                if word1 in vocab_index:
+                    index1 = vocab_index[word1]
+                else:
+                    index1 = None
+                for word2 in words[word_i+1:30]:
+                    if word2 in vocab_index:
+                        index2 = vocab_index[word2]
+                    else:
+                        index2 = None
+                    if index1 is None or index2 is None:
+                        npmi = 0.0
+                    else:
+                        col1 = np.array(ref_counts[:, index1].todense() > 0, dtype=int)
+                        col2 = np.array(ref_counts[:, index2].todense() > 0, dtype=int)
+                        sum1 = col1.sum()
+                        sum2 = col2.sum()
+                        interaction = np.sum(col1 * col2)
+                        assert self.model._ref_interaction[index1, index2] == np.log10(interaction)
+                        assert self.model._ref_doc_sum[index1] == sum1
+                        assert self.model._ref_doc_sum[index2] == sum2
+                        if interaction == 0:
+                            assert self.model._npmi[index1, index2] == 0.0
+                            _npmi = 0.0
+                        else:
+                            expected_numerator = np.log10(n_docs) + np.log10(interaction) - np.log10(sum1) - np.log10(sum2)
+                            numerator = np.log10(self.model.n_docs) + self.model._npmi_numerator[index1, index2]
+                            assert np.isclose(expected_numerator, numerator)
+                            expected_denominator = np.log10(n_docs) - np.log10(interaction)
+                            denominator = np.log10(self.model.n_docs) - self.model._npmi_denominator[index1, index2]
+                            assert np.isclose(expected_denominator, denominator)
+                            _npmi = expected_numerator / expected_denominator
+                    npmi_vals.append(_npmi)
+            npmi_means.append(np.mean(npmi_vals))
+        assert np.isclose(npmi, np.mean(npmi_means))
+        
