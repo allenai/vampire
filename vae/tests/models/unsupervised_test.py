735484ab6861263d1c72427f70d55b0a3e6a0c00
Suchin Gururangan <github@suchin.io>
2019-02-09 15:30:59 -0800

    incorporated tam comments, all checks pass

--- vae/tests/models/unsupervised_test.py
+++ vae/tests/models/unsupervised_test.py
@@ -23,9 +23,9 @@ class TestUnsupervised(ModelTestCase):
         save_dir = self.TEST_DIR / "save_and_load_test"
         model = train_model_from_file(self.param_file, save_dir, overrides="")
 
-        topics = [(1, ["great", "movie"]), (2, ["bad", "film"])]
-        
-        npmi = model.compute_npmi(topics, num_words=30)
+        topics = [(1, ["great", "movie", "film", "amazing", "wow", "best", "ridiculous", "ever", "good", "incredible", "positive"]),
+                  (2, ["bad", "film", "worst", "negative", "movie", "ever", "not", "any", "gross", "boring"])]
+        npmi = model.compute_npmi(topics, num_words=10)
 
         ref_vocab = model._ref_vocab
         ref_counts = model._ref_count_mat
@@ -37,31 +37,31 @@ class TestUnsupervised(ModelTestCase):
         for topic in topics:
             words = topic[1]
             npmi_vals = []
-            for word_i, word1 in enumerate(words[:30]):
+            for word_i, word1 in enumerate(words[:10]):
                 if word1 in vocab_index:
                     index1 = vocab_index[word1]
                 else:
                     index1 = None
-                for word2 in words[word_i+1:30]:
+                for word2 in words[word_i+1:10]:
                     if word2 in vocab_index:
                         index2 = vocab_index[word2]
                     else:
                         index2 = None
                     if index1 is None or index2 is None:
-                        npmi = 0.0
+                        _npmi = 0.0
                     else:
                         col1 = np.array(ref_counts[:, index1].todense() > 0, dtype=int)
                         col2 = np.array(ref_counts[:, index2].todense() > 0, dtype=int)
                         sum1 = col1.sum()
                         sum2 = col2.sum()
                         interaction = np.sum(col1 * col2)
-                        assert model._ref_interaction[index1, index2] == np.log10(interaction)
-                        assert model._ref_doc_sum[index1] == sum1
-                        assert model._ref_doc_sum[index2] == sum2
                         if interaction == 0:
-                            assert model._npmi[index1, index2] == 0.0
+                            assert model._npmi_numerator[index1, index2] == 0.0 and model._npmi_denominator[index1, index2] == 0.0
                             _npmi = 0.0
                         else:
+                            assert model._ref_interaction[index1, index2] == np.log10(interaction)
+                            assert model._ref_doc_sum[index1] == sum1
+                            assert model._ref_doc_sum[index2] == sum2
                             expected_numerator = np.log10(n_docs) + np.log10(interaction) - np.log10(sum1) - np.log10(sum2)
                             numerator = np.log10(model.n_docs) + model._npmi_numerator[index1, index2]
                             assert np.isclose(expected_numerator, numerator)
@@ -72,4 +72,3 @@ class TestUnsupervised(ModelTestCase):
                     npmi_vals.append(_npmi)
             npmi_means.append(np.mean(npmi_vals))
         assert np.isclose(npmi, np.mean(npmi_means))
-        
