6541dee8cd71fd01f65658d3f3d3284fdcd33889
Suchin Gururangan <github@suchin.io>
2019-02-10 22:59:34 -0800

    added throttled results

--- beaker/search_environments.py
+++ beaker/search_environments.py
@@ -1,6 +1,56 @@
 from beaker.random_search import RandomSearch
 
 
+CLASSIFIER_500_SEARCH = {
+        "EMBEDDING_DIM": RandomSearch.random_choice(50, 128, 300, 512),
+        "SEED": 1423,
+        "TRAIN_PATH": "s3://suching-dev/imdb/train.jsonl",
+        "DEV_PATH": "s3://suching-dev/imdb/dev.jsonl",
+        "REFERENCE_COUNTS": "s3://suching-dev/valid_npmi_reference/train.npz",
+        "REFERENCE_VOCAB": "s3://suching-dev/valid_npmi_reference/train.vocab.json",
+        "STOPWORDS_PATH": "s3://suching-dev/stopwords/snowball_stopwords.txt",
+        "VOCAB_SIZE": 30000,
+        "THROTTLE": 500,
+        "USE_SPACY_TOKENIZER": 1,
+        "ADD_ELMO": 0,
+        "ADD_VAE": 0,
+        "LEARNING_RATE": RandomSearch.random_choice(1, 5, 10),
+        "DROPOUT": RandomSearch.random_choice(0, 2, 5),
+        "NUM_FILTERS": RandomSearch.random_choice(128, 156, 512),
+        "MAX_FILTER_SIZE":  RandomSearch.random_choice(5, 7, 10),
+        "NUM_ENCODER_LAYERS": RandomSearch.random_choice(1, 2, 3),
+        "AGGREGATIONS": RandomSearch.random_subset("final_state", "maxpool", "meanpool"),
+        "CLF_HIDDEN_DIM": RandomSearch.random_choice(64, 128, 512, 1024, 2048),
+        "CLASSIFIER": RandomSearch.random_choice("lstm", "boe", "lr", "cnn"),
+        "NUM_GPU": 1
+}
+
+
+CLASSIFIER_200_SEARCH = {
+        "EMBEDDING_DIM": RandomSearch.random_choice(50, 128, 300, 512),
+        "SEED": 1110101010,
+        "TRAIN_PATH": "s3://suching-dev/imdb/train.jsonl",
+        "DEV_PATH": "s3://suching-dev/imdb/dev.jsonl",
+        "REFERENCE_COUNTS": "s3://suching-dev/valid_npmi_reference/train.npz",
+        "REFERENCE_VOCAB": "s3://suching-dev/valid_npmi_reference/train.vocab.json",
+        "STOPWORDS_PATH": "s3://suching-dev/stopwords/snowball_stopwords.txt",
+        "VOCAB_SIZE": 30000,
+        "THROTTLE": 200,
+        "USE_SPACY_TOKENIZER": 1,
+        "ADD_ELMO": 0,
+        "ADD_VAE": 0,
+        "LEARNING_RATE": RandomSearch.random_choice(1, 5, 10),
+        "DROPOUT": RandomSearch.random_choice(0, 2, 5),
+        "NUM_FILTERS": RandomSearch.random_choice(128, 156, 512),
+        "MAX_FILTER_SIZE":  RandomSearch.random_choice(5, 7, 10),
+        "NUM_ENCODER_LAYERS": RandomSearch.random_choice(1, 2, 3),
+        "AGGREGATIONS": RandomSearch.random_subset("final_state", "maxpool", "meanpool"),
+        "CLF_HIDDEN_DIM": RandomSearch.random_choice(64, 128, 512, 1024, 2048),
+        "CLASSIFIER": RandomSearch.random_choice("lstm", "boe", "lr", "cnn"),
+        "NUM_GPU": 1
+}
+
+
 CLASSIFIER_SEARCH = {
         "EMBEDDING_DIM": RandomSearch.random_choice(50, 128, 300, 512),
         "SEED": 42,
@@ -120,5 +170,7 @@ SEARCH_ENVIRONMENTS = {
             'UNSUPERVISED_VAE_SEARCH': UNSUPERVISED_VAE_SEARCH,
             "CLASSIFIER_SEARCH": CLASSIFIER_SEARCH,
             "CNN_CLASSIFIER_SEARCH": CNN_CLASSIFIER_SEARCH,
-            "LSTM_CLASSIFIER_SEARCH": LSTM_CLASSIFIER_SEARCH
+            "LSTM_CLASSIFIER_SEARCH": LSTM_CLASSIFIER_SEARCH,
+            "CLASSIFIER_200_SEARCH": CLASSIFIER_200_SEARCH,
+            "CLASSIFIER_500_SEARCH": CLASSIFIER_500_SEARCH
 }
---
+++ training_config/best/imdb/200/lr_classifier.jsonnet
@@ -0,0 +1,74 @@
+local CUDA_DEVICE =
+  if std.parseInt(std.extVar("NUM_GPU")) == 0 then
+    -1
+  else if std.parseInt(std.extVar("NUM_GPU")) > 1 then
+    std.range(0, std.extVar("NUM_GPU") - 1)
+  else if std.parseInt(std.extVar("NUM_GPU")) == 1 then
+    0;
+
+{
+    "dataset_reader": {
+        "type": "semisupervised_text_classification_json",
+        "lazy": false,
+        "sample": "200",
+        "sequence_length": 400,
+        "token_indexers": {
+            "tokens": {
+                "type": "single_id",
+                "lowercase_tokens": true,
+                "namespace": "classifier"
+            }
+        },
+        "tokenizer": {
+            "word_splitter": "spacy"
+        }
+    },
+    "iterator": {
+        "type": "basic",
+        "batch_size": 128
+    },
+    "model": {
+        "type": "classifier",
+        "dropout": 0.5,
+        "input_embedder": {
+            "token_embedders": {
+                "tokens": {
+                    "type": "bag_of_word_counts",
+                    "ignore_oov": "true",
+                    "vocab_namespace": "classifier"
+                }
+            }
+        }
+    },
+    "train_data_path": "s3://suching-dev/imdb/train.jsonl",
+    "validation_data_path": "s3://suching-dev/imdb/dev.jsonl",
+    "trainer": {
+        "cuda_device": CUDA_DEVICE,
+        "num_epochs": 200,
+        "optimizer": {
+            "type": "adam",
+            "lr": 0.001
+        },
+        "patience": 20,
+        "validation_metric": "+accuracy"
+    },
+    "datasets_for_vocab_creation": [
+        "train"
+    ],
+    "validation_dataset_reader": {
+        "type": "semisupervised_text_classification_json",
+        "lazy": false,
+        "sample": null,
+        "sequence_length": 400,
+        "token_indexers": {
+            "tokens": {
+                "type": "single_id",
+                "lowercase_tokens": true,
+                "namespace": "classifier"
+            }
+        },
+        "tokenizer": {
+            "word_splitter": "spacy"
+        }
+    }
+}
