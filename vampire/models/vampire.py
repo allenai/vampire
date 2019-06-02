44b6b3ed501fb77a247c8ec0d4bebaeec2268c6d
Suchin Gururangan <github@suchin.io>
2019-06-02 16:45:42 -0700

    update
    Former-commit-id: 5f6f66d8810284204a58c67bbc998b849db50021

--- vampire/models/vampire.py
+++ vampire/models/vampire.py
@@ -300,7 +300,8 @@ class VAMPIRE(Model):
 
     @overrides
     def forward(self,  # pylint: disable=arguments-differ
-                tokens: Dict[str, torch.LongTensor],
+                tokens: Dict[str, torch.IntTensor] = None, 
+                vec: torch.Tensor = None,
                 label: torch.Tensor = None,  # pylint: disable=unused-argument
                 covariate: torch.Tensor = None,
                 metadata: List[Dict[str, Any]] = None,  # pylint: disable=unused-argument
@@ -323,8 +324,10 @@ class VAMPIRE(Model):
                                    sigmoid_weight_1=self._sigmoid_weight_1,
                                    sigmoid_weight_2=self._sigmoid_weight_2)
 
-        embedded_tokens = self._bow_embedding(tokens['tokens'])
-
+        if tokens:
+            embedded_tokens = self._bow_embedding(tokens['tokens'])
+        else:
+            embedded_tokens = vec
         # Encode the text into a shared representation for both the VAE
         # and downstream classifiers to use.
         encoder_output = self.vae.encoder(embedded_tokens)
@@ -363,7 +366,6 @@ class VAMPIRE(Model):
 
         output_dict['activations'] = activations
 
-        output_dict['mask'] = get_text_field_mask(tokens)
         # Update metrics
         self.metrics['nkld'](-torch.mean(negative_kl_divergence))
         self.metrics['nll'](-torch.mean(reconstruction_loss))
