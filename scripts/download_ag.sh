eb7f50ec55c6261263b3a70e282b4474a7fcb556
Suchin Gururangan <github@suchin.io>
2019-06-04 11:30:02 -0700

    update README
    Former-commit-id: 9a443bdea22e8edd551f54f23264514083aef3ac

--- scripts/download_ag.sh
+++ scripts/download_ag.sh
@@ -1,4 +1,3 @@
-
 mkdir -p $(pwd)/examples/ag
 curl -Lo $(pwd)/examples/ag/train.jsonl https://s3-us-west-2.amazonaws.com/allennlp/datasets/ag-news/train.jsonl
 curl -Lo $(pwd)/examples/ag/dev.jsonl https://s3-us-west-2.amazonaws.com/allennlp/datasets/ag-news/dev.jsonl
