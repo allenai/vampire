df57404bc33580e71ab743cbf4b62fcc0b2f95c5
Shuning Jin <jinxx596@d.umn.edu>
2020-04-19 17:04:22 -0500

    append seed to config.json
    Former-commit-id: 74db021846b3153e12cfa6b3c2a8af0eb7078e4d

--- scripts/train.py
+++ scripts/train.py
@@ -26,20 +26,20 @@ def main():
     parser.add_argument('-r', '--recover', action='store_true', help = "recover saved model")
     parser.add_argument('-d', '--device', type=str, required=False, help = "device to run model on")
     parser.add_argument('-x', '--seed', type=str, required=False, help = "seed to run on")
-    
+
 
     args = parser.parse_args()
 
     env = ENVIRONMENTS[args.environment.upper()]
 
-    
+
     space = HyperparameterSearch(**env)
 
     sample = space.sample()
-    
+
     for key, val in sample.items():
         os.environ[key] = str(val)
-    
+
     if args.device:
         os.environ['CUDA_DEVICE'] = args.device
 
@@ -56,13 +56,27 @@ def main():
             "-s",
             args.serialization_dir
             ]
-    
+
     if args.seed:
         allennlp_command[-1] = allennlp_command[-1] + "_" + args.seed
 
     if args.recover:
+        def append_seed_to_config(seed, serialization_dir):
+            seed = str(seed)
+            seed_dict = {"pytorch_seed": seed,
+                         "random_seed": seed,
+                         "numpy_seed": seed}
+            config_path = os.path.join(serialization_dir, 'config.json')
+            with open(config_path, 'r+') as f:
+                config_dict = json.load(f)
+                seed_dict.update(config_dict)
+                f.seek(0)
+                json.dump(seed_dict, f, indent=4)
+
+        append_seed_to_config(seed=args.seed, serialization_dir=allennlp_command[-1])
+
         allennlp_command.append("--recover")
-    
+
     if os.path.exists(allennlp_command[-1]) and args.override:
         print(f"overriding {allennlp_command[-1]}")
         shutil.rmtree(allennlp_command[-1])
