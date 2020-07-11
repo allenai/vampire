99ee579389cd6f376adcfdf27b82cd8de4fb5c96
kernelmachine <sgrangan01@gmail.com>
2020-07-10 17:22:50 -0700

    added vampire predictor
    Former-commit-id: c8dd37d061f698083739a28b005ebbceb5d71869

---
+++ vampire/predictors/vampire.py
@@ -0,0 +1,24 @@
+from copy import deepcopy
+from typing import List, Dict
+
+from overrides import overrides
+import numpy
+
+from allennlp.common.util import JsonDict, sanitize
+from allennlp.data import Instance
+from allennlp.predictors.predictor import Predictor
+from allennlp.data.fields import LabelField
+from allennlp.data.tokenizers import Token
+
+
+@Predictor.register("vampire")
+class VampirePredictor(Predictor):
+    
+    @overrides
+    def _json_to_instance(self, json_dict: JsonDict) -> Instance:
+        """
+        Expects JSON that looks like ``{"sentence": "..."}``.
+        Runs the underlying model, and adds the ``"label"`` to the output.
+        """
+        sentence = [Token(x) for x in json_dict["text"].split(" ")]
+        return self._dataset_reader.text_to_instance(sentence)
