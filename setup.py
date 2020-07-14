c3f3e3849211d094f7c6783a6d09021bc6756253
kernelmachine <sgrangan01@gmail.com>
2020-07-14 16:56:45 -0700

    added setup.py
    Former-commit-id: d9545027965fa73bbf2e4c732604dcca88c9e735

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
-def parse_requirements(filename):
-    """ load requirements from a pip requirements file """
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session='none')
 
 # reqs is a list of requirement
 dependencies = [str(ir.req) for ir in install_reqs]
