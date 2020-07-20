4933c38154cbb9f6d966f7c2e8008698b9a45517
kernelmachine <sgrangan01@gmail.com>
2020-07-20 16:37:59 -0700

    added READMEs
    Former-commit-id: ff22000faadb732326337cc5b710c0dea448018b

--- vampire/api/model.py
+++ vampire/api/model.py
@@ -236,7 +236,7 @@ class VampireModel(object):
         else:
             self.model.eval()
             with torch.no_grad():
-                results = self.model(torch.IntTensor(input_))
+                results = self.model(torch.Tensor(input_))
             import ipdb; ipdb.set_trace()
             # for row in input_:
             #     instances.append(self.model._array_to_instance(row))
