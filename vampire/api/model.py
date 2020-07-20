bc52917ec0e6a8cdc70078003b69b01e4531de08
kernelmachine <sgrangan01@gmail.com>
2020-07-20 16:59:58 -0700

    added READMEs
    Former-commit-id: d65baba68b42f8e881a1991e7a9a5625da1a5e42

--- vampire/api/model.py
+++ vampire/api/model.py
@@ -237,9 +237,9 @@ class VampireModel(object):
                 results = [self.model(torch.Tensor(input_).to(self.device))]
         for output in results:
             if scalar_mix:
-                output = (torch.Tensor(output['encoder_layer_0']).unsqueeze(0)
-                            + -20 * torch.Tensor(output['encoder_layer_1']).unsqueeze(0)
-                            + torch.Tensor(output['theta']).unsqueeze(0))
+                output = (torch.Tensor(output['encoder_layer_0'].cpu()).unsqueeze(0)
+                            + -20 * torch.Tensor(output['encoder_layer_1'].cpu()).unsqueeze(0)
+                            + torch.Tensor(output['theta'].cpu()).unsqueeze(0))
             yield output
 
 if __name__ == '__main__':
